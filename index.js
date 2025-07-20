14e6fbd2f8b787190c4a986bb7b8a226f45b222a
Linkedbump <linkedbump@gmail.com>
2025-07-20 20:42:27 +0200

    fix: servidor express render

--- index.js
+++ index.js
@@ -1,40 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
-const {setGlobalOptions} = require("firebase-functions");
-const {onRequest} = require("firebase-functions/https");
-const logger = require("firebase-functions/logger");
-
-// For cost control, you can set the maximum number of containers that can be
-// running at the same time. This helps mitigate the impact of unexpected
-// traffic spikes by instead downgrading performance. This limit is a
-// per-function limit. You can override the limit for each function using the
-// `maxInstances` option in the function's options, e.g.
-// `onRequest({ maxInstances: 5 }, (req, res) => { ... })`.
-// NOTE: setGlobalOptions does not apply to functions using the v1 API. V1
-// functions should each use functions.runWith({ maxInstances: 10 }) instead.
-// In the v1 API, each function can only serve one request per container, so
-// this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
-
-const functions = require("firebase-functions");
+const express = require("express");
+const bodyParser = require("body-parser");
 const axios = require("axios");
 
-exports.npc = functions.https.onRequest(async (req, res) => {
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/npc", async (req, res) => {
   const { systemPrompt, userInput } = req.body;
   console.log("BODY RECIBIDO:", req.body);
 
@@ -55,6 +26,7 @@ exports.npc = functions.https.onRequest(async (req, res) => {
         }
       }
     );
+
     console.log("RESPUESTA OPENAI:", response.data);
 
     res.json({ reply: response.data.choices[0].message.content });
@@ -64,3 +36,8 @@ exports.npc = functions.https.onRequest(async (req, res) => {
   }
 });
 
+// Esto es lo que mantiene vivo el servidor en Render
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor escuchando en puerto ${PORT}`);
+});
