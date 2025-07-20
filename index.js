77b6a508824cd0a528bda4ad512dcf54ee72ea6e
Linkedbump <linkedbump@gmail.com>
2025-07-20 21:05:37 +0200

    fix: habilitar CORS

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const cors = require("cors"); 
 const axios = require("axios");
 
 const app = express();
+app.use(cors()); 
 app.use(bodyParser.json());
 
 app.post("/npc", async (req, res) => {
